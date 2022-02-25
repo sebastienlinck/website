d9f14b5426260b6fb3cbda11de16e7a1349fb5e8
Sébastien Linck <linck.sebastien@gmail.Com>
2022-02-25 15:13:55 +0100

    uicons

--- pages/contact.php
+++ pages/contact.php
@@ -4,7 +4,7 @@
 		<article>
 			<img class="img-round" src="img/linck.jpg" alt="Sébastien Linck" title="Sébastien Linck">
 		</article>
-		
+
 		<article>
 			<h4>Sébastien Linck</h4>
 			<br>
@@ -16,24 +16,22 @@
 			<a target="_blank" href="https://fr.linkedin.com/in/slinck/"><img class="social-icons" src="img/linkedin.png" alt="Profil LinkedIn" title="Profil LinkedIn"></a>
 			<a target="_blank" href="https://www.researchgate.net/profile/Sebastien_Linck"><img class="social-icons" src="img/researchgate.png" alt="Profil ResearchGate" title="Profil ResearchGate"></a>
 		</article>
-		
+
 		<article>
-			<?php 
-				if(isset($_SESSION['success']) AND $_SESSION['success'] == 1)
-				{
-					echo "Votre message a bien été transmis !";
-				}
-				if (isset($_SESSION['success']) AND $_SESSION['success'] == 0)
-				{
-					echo "Veuillez remplir le captcha !";
-				}
+			<?php
+			if (isset($_SESSION['success']) and $_SESSION['success'] == 1) {
+				echo "Votre message a bien été transmis !";
+			}
+			if (isset($_SESSION['success']) and $_SESSION['success'] == 0) {
+				echo "Veuillez remplir le captcha !";
+			}
 			?>
 			<form id="contact-form" action="pages/send_form.php" method="post">
-				<input required type="text" name="name" id="name" value="<?php echo isset($_SESSION['inputs']['name'])? $_SESSION['inputs']['name'] : ''; ?>" placeholder="Votre nom">
-				<input required type="email" name="email" id="email" value="<?php echo isset($_SESSION['inputs']['email'])? $_SESSION['inputs']['email'] : ''; ?>" placeholder="Votre email">
-				<textarea rows="4" required id="message" name="message" placeholder="Votre message"><?php echo isset($_SESSION['inputs']['message'])? $_SESSION['inputs']['message'] : ''; ?></textarea>
+				<input required type="text" name="name" id="name" value="<?php echo isset($_SESSION['inputs']['name']) ? $_SESSION['inputs']['name'] : ''; ?>" placeholder="Votre nom">
+				<input required type="email" name="email" id="email" value="<?php echo isset($_SESSION['inputs']['email']) ? $_SESSION['inputs']['email'] : ''; ?>" placeholder="Votre email">
+				<textarea rows="4" required id="message" name="message" placeholder="Votre message"><?php echo isset($_SESSION['inputs']['message']) ? $_SESSION['inputs']['message'] : ''; ?></textarea>
 				<button class="g-recaptcha" data-sitekey="6LcBwRgUAAAAADQmcYmD0Tr0LTMM78W0k3Qmabl7" data-callback='onSubmit'>Envoyer</button>
-			</form>	
+			</form>
 		</article>
 	</div>
-</section> 	
+</section>
--- pages/footer.php
+++ pages/footer.php
@@ -1,38 +1,25 @@
 <?php
-	function array_prefix_values($prefix, $array)
-	{
-		$callback = create_function('$s','return "'.$prefix.'".$s;');
-		return array_map($callback,$array);
-	}
-	
-	function get_last_update()
-	{
-		if (func_num_args()< 1) return 0;
-		$dirs = func_get_args();
-		$files = array();
-		foreach ($dirs as $dir)
-		{
-			$subfiles = scandir($dir);
-			$subfiles = array_prefix_values($dir,$subfiles);
-			$subfiles = array_filter($subfiles,"is_file");
-			$files = array_merge($files,$subfiles);
-		}
-		$maxtimestamp = 0;
-		foreach ($files as $file)
-		{
-			$timestamp = filemtime($file);
-			if ($timestamp> $maxtimestamp)
-			{
-				$maxtimestamp = $timestamp;
+function mostRecentModifiedFileTime($dirName, $doRecursive)
+{
+	$d = dir($dirName);
+	$lastModified = 0;
+	while ($entry = $d->read()) {
+		if ($entry != "." && $entry != "..") {
+			if (!is_dir($dirName . "/" . $entry)) {
+				$currentModified = filemtime($dirName . "/" . $entry);
+			} else if ($doRecursive && is_dir($dirName . "/" . $entry)) {
+				$currentModified = mostRecentModifiedFileTime($dirName . "/" . $entry, true);
+			}
+			if ($currentModified > $lastModified) {
+				$lastModified = $currentModified;
 			}
 		}
-		error_reporting(E_ALL);
-		ini_set('display_errors', TRUE);
-		setlocale (LC_TIME, 'fr_FR.utf8','fr_FR','fra');//pas en localhost
-		return ucwords(strftime("%B %Y",$maxtimestamp));
 	}
-	
+	$d->close();
+	return ucwords(strftime("%B %Y", $lastModified));
+}
+
 ?>
 <footer>
-	Sébastien Linck - <?php echo get_last_update(".", "./css/", "./pages/");?> - <a href="mentions-legales">Mentions légales</a>
+	Sébastien Linck - <?php echo mostRecentModifiedFileTime(".", 1); ?> - <a href="mentions-legales">Mentions légales</a>
 </footer>
