a437f7432eff1636f19e4d2f723c101b765a61d7
Sébastien Linck <linck.sebastien@gmail.Com>
2022-03-01 16:12:00 +0100

    Update outils.js

--- js/outils.js
+++ js/outils.js
@@ -2,11 +2,10 @@ function onSubmit(token) {
 	document.getElementById("contact-form").submit();
 }
 
-window.addEventListener('scroll', function() {
-	console.log(document.documentElement.scrollTop);
+window.addEventListener('scroll', event => {
 	if (document.documentElement.scrollTop > 300) {
 		document.querySelector('.enhaut').classList.add('visible');
-		} 
+	} 
 	else {
 		document.querySelector('.enhaut').classList.remove('visible');
 	}
@@ -16,4 +15,9 @@ document.querySelectorAll('.ssmenu').forEach(item => {
 	item.addEventListener('click', event => {
 		item.nextElementSibling.classList.toggle('visible');
 	})
+})
+
+document.querySelector('.enhaut').addEventListener('click', function(e) {
+	e.preventDefault();
+	window.scrollTo({top: 0, behavior: 'smooth'});
 })
