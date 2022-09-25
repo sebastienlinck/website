4ccc1f7c2b3df0dfd5cdec3893707586225bbfe4
Sébastien Linck <linck.sebastien@gmail.com>
2022-09-25 12:40:01 +0200

    Update footer.php
    PHP 8 compatibility

--- pages/footer.php
+++ pages/footer.php
@@ -1,24 +1,25 @@
 <?php
-function mostRecentModifiedFileTime($dirName, $doRecursive)
-{
-	$d = dir($dirName);
-	$lastModified = 0;
-	while ($entry = $d->read()) {
-		if ($entry != "." && $entry != "..") {
-			if (!is_dir($dirName . "/" . $entry)) {
-				$currentModified = filemtime($dirName . "/" . $entry);
-			} else if ($doRecursive && is_dir($dirName . "/" . $entry)) {
-				$currentModified = mostRecentModifiedFileTime($dirName . "/" . $entry, true);
-			}
-			if ($currentModified > $lastModified) {
-				$lastModified = $currentModified;
+	$fmt = datefmt_create('fr_FR',IntlDateFormatter::FULL,IntlDateFormatter::FULL,'Europe/Paris',IntlDateFormatter::GREGORIAN,'MMMM YYYY');
+	function mostRecentModifiedFileTime($dirName, $doRecursive)
+	{
+		$d = dir($dirName);
+		$lastModified = 0;
+		while ($entry = $d->read()) {
+			if ($entry != "." && $entry != "..") {
+				if (!is_dir($dirName . "/" . $entry)) {
+					$currentModified = filemtime($dirName . "/" . $entry);
+					} else if ($doRecursive && is_dir($dirName . "/" . $entry)) {
+					$currentModified = mostRecentModifiedFileTime($dirName . "/" . $entry, true);
+				}
+				if ($currentModified > $lastModified) {
+					$lastModified = $currentModified;
+				}
 			}
 		}
+		$d->close();
+		return  $lastModified;
 	}
-	$d->close();
-	return ucwords(strftime("%B %Y", $lastModified));
-}
 ?>
 <footer>
-	Sébastien Linck - <?php echo mostRecentModifiedFileTime(".", 1); ?> - <a href="mentions-legales">Mentions légales</a>
+	Sébastien Linck - <?php  echo ucwords(datefmt_format($fmt, mostRecentModifiedFileTime(".", 1))); ?> - <a href="mentions-legales">Mentions légales</a>
 </footer>
