371d249522496f72d32e9655d2af4ced8ab0ac84
sebastienlinck <55988723+sebastienlinck@users.noreply.github.com>
2023-01-23 16:22:09 +0100

    Update index.php

--- index.php
+++ index.php
@@ -37,6 +37,8 @@
 		<link rel='stylesheet' href='https://cdn-uicons.flaticon.com/uicons-bold-rounded/css/uicons-bold-rounded.css'>
 		<link rel='stylesheet' href='https://cdn-uicons.flaticon.com/uicons-regular-rounded/css/uicons-regular-rounded.css'>
 		<link rel="stylesheet" href="./css/style.css">
+		<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0], j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-M85B535');</script>
 		<!-- Global site tag (gtag.js) - Google Analytics -->
 		<script async src="https://www.googletagmanager.com/gtag/js?id=UA-72813591-1"></script>
 		<script>
@@ -50,20 +52,11 @@
 		<script src="https://www.google.com/recaptcha/api.js" async defer></script>
 		<link rel="manifest" href="/slinck.webmanifest">
 		<meta name="google-site-verification" content="W4B7FHprbWn7QDiEttuBXnN7X6bL2P1SWMmNO2c8Tlw" />
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-CX6WD4QDPN"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
 
-  gtag('config', 'G-CX6WD4QDPN');
-		</script>
 	</head>
 	
 	<body>
-		<noscript>
-			<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-M85B535" height="0" width="0" style="display:none;visibility:hidden"></iframe>
-		</noscript>
+	<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-M85B535" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 		<div id="page">
 			<header>
 				<h1>Sébastien Linck</h1>
