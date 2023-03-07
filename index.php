f0731eca803dd624daf15fd4395550bdb6cdcb8d
sebastienlinck <55988723+sebastienlinck@users.noreply.github.com>
2023-03-07 11:46:48 +0100

    Update index.php

--- index.php
+++ index.php
@@ -37,7 +37,11 @@
 		<link rel="apple-touch-icon" sizes="152x152" href="./img/favicon.png">
 		<link rel="stylesheet" href="./css/style.css">
 		<!-- Google Tag Manager -->
-		<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0], j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-M85B535');</script>
+		<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+		new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+		j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+		'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+		})(window,document,'script','dataLayer','GTM-M85B535');</script>
 		<!-- End Google Tag Manager -->
 		<!-- Google tag (gtag.js) -->
 		<script async src="https://www.googletagmanager.com/gtag/js?id=G-CX6WD4QDPN"></script>
