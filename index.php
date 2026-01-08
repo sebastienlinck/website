d5d064f6a6094e7462a4a39134708099549048bd
Sebastien Linck <linck.sebastien@gmail.com>
2026-01-08 16:57:00 +0100

    Mise à jour de l'intégration de Google Tag Manager avec gtag.js pour un suivi amélioré des utilisateurs.

--- index.php
+++ index.php
@@ -121,24 +121,20 @@ $canonicalPage = $page !== 'accueil' ? '/' . htmlspecialchars($page, ENT_QUOTES,
 	<meta property="og:image" content="./img/linck.webp">
 	<link rel="icon" type="image/svg" sizes="256x256" href="./img/favicon.svg">
 	<link rel="apple-touch-icon" href="./img/favicon.svg">
-	<!-- Google Tag Manager -->
+
+	<!-- Google tag (gtag.js) -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=G-CX6WD4QDPN"></script>
 	<script>
-		(function(w, d, s, l, i) {
-			w[l] = w[l] || [];
-			w[l].push({
-				'gtm.start': new Date().getTime(),
-				event: 'gtm.js'
-			});
-			var f = d.getElementsByTagName(s)[0],
-				j = d.createElement(s),
-				dl = l != 'dataLayer' ? '&l=' + l : '';
-			j.async = true;
-			j.src =
-				'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
-			f.parentNode.insertBefore(j, f);
-		})(window, document, 'script', 'dataLayer', 'GTM-M85B535');
+		window.dataLayer = window.dataLayer || [];
+
+		function gtag() {
+			dataLayer.push(arguments);
+		}
+		gtag('js', new Date());
+
+		gtag('config', 'G-CX6WD4QDPN');
 	</script>
-	<!-- End Google Tag Manager -->
+
 	<link rel="stylesheet" href="./css/style.min.css">
 	<link rel="manifest" href="./slinck.webmanifest">
 	<meta name="google-site-verification" content="W4B7FHprbWn7QDiEttuBXnN7X6bL2P1SWMmNO2c8Tlw">
