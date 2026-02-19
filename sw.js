2c84bba39a04fc2369768f02dd51b2516e860c45
Sebastien Linck <linck.sebastien@gmail.com>
2026-02-19 14:20:52 +0100

    Mise à jour de la politique de sécurité du contenu pour inclure le stockage Google et ajout de commentaires explicatifs dans le fichier sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,9 @@
 const CACHE = "pwabuilder-offline";
 
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
+// On essaie de charger Workbox
+importScripts(
+  "https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js",
+);
 
 self.addEventListener("message", (event) => {
   if (event.data && event.data.type === "SKIP_WAITING") {
@@ -8,9 +11,14 @@ self.addEventListener("message", (event) => {
   }
 });
 
-workbox.routing.registerRoute(
-  new RegExp('/*'),
-  new workbox.strategies.StaleWhileRevalidate({
-    cacheName: CACHE
-  })
-);
+// On vérifie que Workbox est bien chargé avant de l'utiliser
+if (workbox) {
+  workbox.routing.registerRoute(
+    new RegExp("/*"),
+    new workbox.strategies.StaleWhileRevalidate({
+      cacheName: CACHE,
+    }),
+  );
+} else {
+  console.log("Workbox n'a pas pu être chargé.");
+}
